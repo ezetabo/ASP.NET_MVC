f9c23ad7beb535b1d5f7ec365cf7a801e6c32333
Ezequiel Taboada <ezequiel.taboada@accenture.com>
2023-02-01 22:10:29 -0300

    inicio

---
+++ Repaso_Formularios/Repaso_Formularios/Controllers/RepasoHtmlController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Repaso_Formularios.Models;
+using System.Diagnostics;
+
+namespace Repaso_Formularios.Controllers
+{
+    public class RepasoHtmlController : Controller
+    {
+        private readonly ILogger<RepasoHtmlController> _logger;
+
+        public RepasoHtmlController(ILogger<RepasoHtmlController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public ActionResult Tabla()
+        {
+            return View();
+        }
+
+        public ActionResult ComboBox()
+        {
+            return View();
+        }
+    }
+}
---
+++ Repaso_Formularios/Repaso_Formularios/wwwroot/js/Sumar.js
@@ -0,0 +1,16 @@
+﻿document.getElementById("btnSumar").onclick = function () {
+
+    var numeroUno = parseFloat(document.getElementById("txtNumeroUno").value);
+    var numeroDos = parseFloat(document.getElementById("txtNumeroDos").value);
+    var suma = numeroUno + numeroDos;
+    document.getElementById("lblResultado").innerHTML = `La suma es: ${suma}`;
+
+}
+
+document.getElementById("btnLimpiar").onclick = function () {
+
+    document.getElementById("txtNumeroUno").value = "";
+    document.getElementById("txtNumeroDos").value = "";
+    document.getElementById("lblResultado").innerHTML = "";
+
+}
